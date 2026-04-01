e7d73c4fb4eb8d41068e42a0592224ff34a2f165
starobj <starobj@starobj.com>
2026-04-01 15:10:08 -0500

    Improve underscore support

--- src/token.rs
+++ src/token.rs
@@ -70,7 +70,7 @@ pub enum Lexeme<'a> {
     * Ukrainian (uk)
     * Vietnamese (vi)
     */
-    #[regex("(([\\_]|\\p{Script=Latin})+)|(一-龯ぁ-んァ-ンー・)")]
+    #[regex("([\\_]|(\\p{Script=Latin})+|(一-龯ぁ-んァ-ンー・))([\\_]|(\\p{Script=Latin})+|(一-龯ぁ-んァ-ンー・))*")]
     Text(&'a str),
 
     // #[regex("[\\:\\;\\.\\+\\-\\*\\/\\=<>]\\{\\}")]
